af4c6e4951c8beffea4a0ac69d5490f4851484d7
jakez <zhu.yifa@husky.neu.edu>
2019-11-27 15:02:01 -0500

    findUserById, userLogin

--- src/main/java/com/example/gamewebappserver/controllers/UserController.java
+++ src/main/java/com/example/gamewebappserver/controllers/UserController.java
@@ -2,10 +2,13 @@ package com.example.gamewebappserver.controllers;
 
 import com.example.gamewebappserver.models.User;
 import com.example.gamewebappserver.repositories.UserRepository;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +29,28 @@ public class UserController {
   @GetMapping("/users")
   public List<User> findAllUsers() {
     return(List<User>) repository.findAll();
+  }
+
+  @GetMapping("/users/{uid}")
+  public User findUserById(
+      @PathVariable("uid") int uid) {
+    Optional<User> c = repository.findById(uid);
+    if (c.isPresent()) {
+      return c.get();
+    }
+    return null;
+  }
+
+  @GetMapping("/login/{usn}/{psd}")
+  public Boolean userLogin(
+      @PathVariable("usn") String usn,
+  @PathVariable("psd") String psd){
+    if (repository.verifyPassword(usn).getPassword().equals(psd)){
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
 
--- src/main/java/com/example/gamewebappserver/repositories/UserRepository.java
+++ src/main/java/com/example/gamewebappserver/repositories/UserRepository.java
@@ -2,8 +2,14 @@ package com.example.gamewebappserver.repositories;
 
 import com.example.gamewebappserver.models.User;
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository
     extends CrudRepository<User, Integer> {
+
+  @Query("select user from User user where user.username=:usn")
+  public User verifyPassword(
+      @Param("usn") String usn);
 }
